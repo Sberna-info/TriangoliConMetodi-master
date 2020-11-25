c0df80c17eb1065594a98a2c59642c6763d0075d
andry <andry@DESKTOP-SENM7HG>
2020-11-25 13:10:54 +0100

    Ho modificato il programma levando il null.

--- ClassTriangoli/MetodiTriangoli.cs
+++ ClassTriangoli/MetodiTriangoli.cs
@@ -18,45 +18,27 @@ namespace ClassTriangoli
         public static string OutputTrangolo(double l1, double l2, double l3)
         {
             string triangolo = null;
-            if (l1 > 0 && l2 > 0 && l3 > 0)
+            if (l1 == l2 && l1 == l3)
             {
-                if (l1 == l2 && l1 == l3)
-                {
-                    triangolo = "equilatero";
-                }
-                else if (l1 == l2 || l1 == l3 || l2 == l3)
-                {
-                    triangolo = "isoscele";
-                }
-                else
-                {
-                    triangolo = "scaleno";
-                }
-                Console.WriteLine($"Il triangolo in questione è {triangolo}");
+                triangolo = "equilatero";
+            }
+            else if (l1 == l2 || l1 == l3 || l2 == l3)
+            {
+                triangolo = "isoscele";
             }
             else
             {
-                Console.WriteLine("uno o più lati non sono validi");
+                triangolo = "scaleno";
             }
+
+
             return triangolo;
         }
 
-        public static double? Perimetro(double l1, double l2, double l3)
+        public static double Perimetro(double l1, double l2, double l3)
         {
-            double? perimetro = null;
-            if(l1 < 0 || l2 < 0 || l3 < 0)
-            {
-                Console.WriteLine("Uno o piu lati non sono validi");
-                return perimetro;
-            }
-            else
-            {
-                perimetro = l1 + l2 + l3;
-                Console.WriteLine($"Il perimitro è {perimetro}");
-                return perimetro;
-            }
+            return l1 + l2 + l3;
 
-            
         }
     }
 }
--- ProgramTriangoli/Program.cs
+++ ProgramTriangoli/Program.cs
@@ -15,7 +15,15 @@ namespace ProgramTriangoli
             double l2 = MetodiTriangoli.Lato();
             double l3 = MetodiTriangoli.Lato();
             string triangolo = MetodiTriangoli.OutputTrangolo(l1, l2, l3);
-            double? perimetro = MetodiTriangoli.Perimetro(l1, l2, l3);
+            double perimetro = MetodiTriangoli.Perimetro(l1, l2, l3);
+            if (l1 < 0 || l2 < 0 || l3 < 0)
+            {
+                Console.WriteLine("Uno o piu lati non sono validi.");
+            }
+            else
+            {
+                Console.WriteLine($"Il triangolo in questione è {triangolo}, ed il perimetro è {perimetro} ");
+            }
             Console.ReadLine();
         }
     }
